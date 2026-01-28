36e28e476e927c4c80a400cd0f759812418d8aab
amysidra <amysidra@gmail.com>
2026-01-28 07:59:58 +0700

    fix test api not found

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Laravel Subdirectory Bridge
+ * This file allows Laravel to run from a subfolder on shared hosting
+ * without needing to include 'public/' in the URL.
+ */
+
+require_once __DIR__ . '/public/index.php';
